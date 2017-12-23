f431804f295d785bcc77e939ae616d21d4a90ad8
funador <jesse.heaslip@gmail.com>
2017-12-22 19:24:15 -0600

    initial commit

---
+++ readme.md
@@ -0,0 +1,132 @@
+# cardano-api
+
+Promise wrapper for the [Cardano API](https://cardanodocs.com/technical/explorer/api/).  
+
+```js
+const { cardano } = require('cardano-api')
+
+const query = {
+    path: 'Ae2tdPwU...fQwGpm' // the address to query
+}
+
+// promise
+cardano.address(query)
+  .then(data => console.log('address', data))
+  .catch(err => console.log('err', err))
+
+// async/await
+const myCardanoFunc = async () => {
+  try {
+    const data = await cardano.address(query)
+    console.log(data)  
+  }
+  catch(err) { console.log(err) }
+}
+
+myCardanoFunc()
+```
+
+# Supported Endpoints
+
+#### cardano.address() - [/api/addresses/summary/{address}](https://cardanodocs.com/technical/explorer/api/#path--api-addresses-summary--address-)
+
+
+
+```js
+const query = {
+    path: 'Ae2tdPwU...fQwGpm' // Required, wallet address to check
+}
+
+cardano.address(query)
+  .then(data => console.log('address', data))
+```
+
+#### cardano.block() - [/api/blocks/summary/{hash}](https://cardanodocs.com/technical/explorer/api/#path--api-blocks-summary--hash-)
+```js
+const query = {
+    path: 'Ae2tdPwU...fQwGpm' // Required, block hash to check
+}
+
+cardano.block(query)
+  .then(data => console.log('block', data))
+```
+
+#### cardano.blocksTotal() - [/api/blocks/pages/total](https://cardanodocs.com/technical/explorer/api/#path--api-blocks-pages-total)
+
+```js
+const query = {
+    pageSize: 3 // Optional
+}
+
+cardano.blockTotal(query)
+  .then(data => console.log('blockTotal', data))
+```
+
+#### cardano.genesisAddress() - [/api/genesis/address/pages](https://cardanodocs.com/technical/explorer/api/#path--api-genesis-address-pages)
+
+```js
+const query = {
+    page: 1,        // Optional
+    pageSize: 3,    // Optional
+    redeemed: true  // Optional
+}
+
+cardano.genesisAddress(query)
+  .then(data => console.log('genesisAddress', data))
+```
+
+#### cardano.genesisPagesTotal() - [/api/genesis/address/pages/total](https://cardanodocs.com/technical/explorer/api/#path--api-genesis-address-pages-total)
+
+```js
+const query = {
+    pageSize: 3,    // Optional
+    redeemed: true  // Optional
+}
+
+cardano.genesisPagesTotal(query)
+  .then(data => console.log('genesisPagesTotal', data))
+```
+
+#### cardano.genesisSummary() - [/api/genesis/address/pages/total](https://cardanodocs.com/technical/explorer/api/#path--api-genesis-summary)
+
+```js
+cardano.genesisSummary()
+  .then(data => console.log('genesisSummary', data))
+```
+
+#### cardano.epoch() - [/api/search/epoch/{epoch}](https://cardanodocs.com/technical/explorer/api/#path--api-search-epoch--epoch-)
+
+```js
+const query = {
+    path: 3,  // Required, epoch to check
+    slot: 24  // Optional
+}
+
+cardano.epoch(query)
+  .then(data => console.log('epoch', data))
+```
+
+#### cardano.transaction() - [/api/txs/summary/{txid}](https://cardanodocs.com/technical/explorer/api/#path--api-txs-summary--txid-)
+
+```js
+const query = {
+    path: 'Ae2tdPwU...fQwGpm' // Required, transaction to check
+}
+
+cardano.transaction(query)
+  .then(data => console.log('transaction', data))
+```
+
+#### cardano.transactionLast() - [/api/txs/last](https://cardanodocs.com/technical/explorer/api/#path--api-txs-last)
+
+```js
+cardano.transactionLast()
+  .then(data => console.log('transactionLast', data))
+```
+
+
+- A couple of the endpoints are represented with breaking changes in the official documentation, assume that more could change in the future
+
+### test
+
+``` npm test```
---
+++ test/test-cardano.js
@@ -0,0 +1,182 @@
+const assert = require('assert')
+const { cardano } = require('../lib/cardano')
+
+describe('cardano api wrapper', () => {
+
+  // Supported Methods
+  // ## address
+  // ## blockPages
+  // ## pagesTotal
+  // ## block
+  // ## genesisAddress
+  // ## genesisPagesTotal
+  // ## genesisSummary
+  // ## epoch
+  // ## transactionLast
+  // ## transaction
+
+  describe('cardano.address()', () => {
+    it('should return information about an address', () => {
+      const query = {
+        path: 'DdzFFzCqrhsoTFwZkQRfYMGMvKwmej3wEbGevViniuuDPTwJoGfLYpWo2zXD8G7cz5ti3m2sz2vsheD4vskNCYVuedsPApxRdDZWJYrm'
+      }
+
+      return cardano.address(query)
+        .then(data => {
+          assert.equal('Right', Object.keys(data)[0])
+          assert.equal(5, Object.keys(data.Right).length)
+        })
+        .catch(err => {
+          console.log('ERR', err)
+        })
+    })
+  })
+
+  describe('cardano.block()', () => {
+    it('should return the given block', () => {
+      const query = {
+        path: '52659df3f8ef7997e0dc8c04b041a956512429b95f4a28275cb53c9f6a195a70',
+      }
+
+      return cardano.block(query)
+        .then(data => {
+          assert.equal('Right', Object.keys(data)[0])
+          assert.equal(4, Object.keys(data.Right).length)
+        })
+        .catch(err => {
+          console.log('ERR', err)
+        })
+    })
+  })
+
+  describe('cardano.blockPages()', () => {
+    it('should return the total amount of blocks', () => {
+      const query = {
+        pageSize: 10,
+        page: 1
+      }
+
+      return cardano.blockPages(query)
+        .then(data => {
+          assert.equal('Right', Object.keys(data)[0])
+        })
+        .catch(err => {
+          console.log('ERR', err)
+        })
+    })
+  })
+
+  describe('cardano.pagesTotal()', () => {
+    it('should return the total amount of blocks', () => {
+      const query = {
+        pageSize: 3,
+      }
+
+      return cardano.pagesTotal(query)
+        .then(data => {
+          assert.equal('Right', Object.keys(data)[0])
+        })
+        .catch(err => {
+          console.log('ERR', err)
+        })
+    })
+  })
+
+  describe('cardano.epoch()', () => {
+    it('should return the specified epoch', () => {
+      const query = {
+        path: 3
+      }
+
+      return cardano.epoch(query)
+        .then(data => {
+          assert.equal('Right', Object.keys(data)[0])
+          assert.equal(2160, data.Right[0])
+        })
+        .catch(err => {
+          console.log('ERR', err)
+        })
+    })
+  })
+
+  describe('cardano.genesisAddress()', () => {
+    it('should return the genesis addresses', () => {
+      const query = {
+        pageSize: 10,
+        redeemed: true,
+        page: 3
+      }
+
+      return cardano.genesisAddress(query)
+        .then(data => {
+          assert.equal('Right', Object.keys(data)[0])
+          assert.equal(10, data.Right.length)
+        })
+        .catch(err => {
+          console.log('ERR', err)
+        })
+    })
+  })
+
+  describe('cardano.genesisPagesTotal()', () => {
+    it('should return the total genesis pages', () => {
+      const query = {
+        pageSize: 10,
+        redeemed: true
+      }
+
+      return cardano.genesisPagesTotal(query)
+        .then(data => {
+          assert.equal('Right', Object.keys(data)[0])
+        })
+        .catch(err => {
+          console.log('ERR', err)
+        })
+    })
+  })
+
+  describe('cardano.genesisSummary()', () => {
+    it('should return the genesis summary', () => {
+
+      return cardano.genesisSummary()
+        .then(data => {
+          assert.equal('Right', Object.keys(data)[0])
+          assert.equal(5, Object.keys(data.Right).length)
+        })
+        .catch(err => {
+          console.log('ERR', err)
+        })
+    })
+  })
+
+  describe('cardano.transaction()', () => {
+    it('should return the specified transaction', () => {
+      const query = {
+        path: 'f3d4686499fd54dcbadc1d501047aadb52b28704fb8126f10f4123d69e3ac99b'
+      }
+
+      return cardano.transaction(query)
+        .then(data => {
+          assert.equal('Right', Object.keys(data)[0])
+          assert.equal(13, Object.keys(data.Right).length)
+        })
+        .catch(err => {
+          console.log('ERR', err)
+        })
+    })
+  })
+
+  describe('cardano.transactionLast()', () => {
+    it('should return the last 20 transactions', () => {
+      return cardano.transactionLast()
+        .then(data => {
+          assert.equal('Right', Object.keys(data)[0])
+          assert.equal(20, data.Right.length)
+        })
+        .catch(err => {
+          console.log('ERR', err)
+        })
+    })
+  }) 
+
+})
